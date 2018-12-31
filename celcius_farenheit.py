2e7deb56b8b348268e5e8e38418de17b8a26a221
benja <benja2255432@gmail.com>
2018-12-31 00:39:29 -0300

    update

--- celcius_farenheit.py
+++ celcius_farenheit.py
@@ -1,20 +1,25 @@
 
 
 f_or_c = "unknown"
-
+temp = ""
 print("\n")
 print(" Conversion F/C ".center(50, "="))
 
 while f_or_c != "f" and f_or_c != "c":
-    f_or_c = input("IS Fahrenheit or Celsius? [F] / [C] ").lower()
+    f_or_c = input("IS Fahrenheit or Celsius? [F] / [C]: ").lower()
 
-temp = float(input("what is your temperature? (only number)"))
+while not temp.isdigit():
+    temp = input("what is your temperature?: ")
+    if not temp.isdigit():
+        print('do not put letters')
+
+print('Temperature Add!')
 
 if f_or_c == "f":
-    conversion = (temp - 32)/1.8
-    print("{}ºC".format(conversion))
+    conversion = (float(temp) - 32)/1.8
+    print("Your temp is the same that {}ºC".format(conversion))
 elif f_or_c == "c":
-    conversion = temp * 1.8 + 32
-    print("{}ºF".format(conversion))
+    conversion = float(temp) * 1.8 + 32
+    print("Your temp is the same that {}ºF".format(conversion))
 
 

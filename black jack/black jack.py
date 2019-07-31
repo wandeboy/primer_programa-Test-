a079f147910f7f4b0613deac7241a291e88e8a8f
benja <benja2255432@gmail.com>
2019-07-31 00:12:46 -0400

    blackjack

--- black jack/black jack.py
+++ black jack/black jack.py
@@ -6,6 +6,9 @@ class Card:
         self.number = number
         self.suit = suit
 
+    def __str__(self):
+        return "{} of {}".format(self.number, self.suit)
+
 
 class Deck:
     # need create: shuffle method
@@ -16,8 +19,12 @@ class Deck:
         self.cards = []
 
         for suit in self.suits:
-            for number in range(1, self.max_number):
+            for number in range(1, self.max_number + 1):
                 self.cards.append(Card(number, suit))
+
+    def __str__(self):
+        str_card = [str(card) for card in self.cards]
+        return "Deck with {} cards:\n{}".format(len(self.cards), ".\n".join(str_card))
 
     def give_random_card(self):
         # move this method to BlackjackGame.
@@ -31,24 +38,76 @@ class Deck:
 
 class Player:
     # Player need money
-    def __int__(self, name, points):
+    def __int__(self, name):
         self.name = name
-        self.points = points
+        self.score = 0
 
 
 class BlackjackGame:
-    def __int__(self, player, deck):
-        self.player = player
-        self.deck = deck
 
-    def player_turn:
-        while palyer
+    card_values = {
+        1: 11,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        11: 10,
+        12: 10,
+        13: 10
+    }
 
-    def who_wins(self):
-        if player.points > 21:
-            player_lose
+    def __int__(self):
+        self.deck = Deck()
+        self.player = Player()
+        self.table_cards = []
 
+    def ask_name(self):
+        name = None
+        while not name.type == str:
+            name = str(input("What's your name?: "))
+        self.player(name)
+
+    def draft_card(self):
+        card = deck.give_random_card()
+        self.table_cards.append(card)
+        print(card)
+
+    def count_table_cards(self):
+        total = 0
+        for card in self.table_cards:
+            if card.number == 1 and total + self.card_values[card.number] > 21:
+                total += 1
+            else:
+                total += self.card_values[card.number]
+        return total
+
+    def player_wants_to_continue(self):
+        response = input("Do you want a card? [Y/N]".upper())
+        return response == "Y"
+
+    def run(self):
+        user_continue = True
+
+        while user_continue and self.count_table_cards() < 21:
+            self.draft_card()
+            user_continue = self.player_wants_to_continue()
+
+        score = self.count_table_cards()
+        print("Your socre is {}".format(score))
+
+        if score > 21:
+            print("You Lose")
 
 
 # need a new class, croupier.
+
+
+if __name__ == "__main__":
+    deck = Deck()
+    print(Deck)
 
